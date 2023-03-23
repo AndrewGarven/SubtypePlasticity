3a4ac4ffee72da1751dbdd2c0e1e419913cef203
AndrewGarven <15ajg6@queensu.ca>
2023-03-23 18:08:53 -0400

    Add HALOSummary.plot_hist()

--- Funcs/MakeGraph.py
+++ Funcs/MakeGraph.py
@@ -1,80 +1,83 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 import pandas as pd
 import itertools
 import networkx as nx
 import matplotlib.pyplot as plt
 from math import sqrt
-from os import path, mkdir
-#from torch_geometric.utils.convert import from_networkx
+from torch_geometric.utils import from_networkx
 
 
 class MakeGraph:
 
-    """MakeGraph is used to generate a networkx object from list of 2D coordinates with nodal information
+    """
+    MakeGraph is used to generate a networkx object from list of 2D coordinates with nodal information
 
         Typical use:
 
-            graph : MakeGraph(datadir, filename, nodeweight, maxlinkingdistance).graph()
+            graph : MakeGraph(data_dir, file_name, node_weight, max_linking_distance).graph()
 
         :datadir: string of directory containing nuclear position data
         :filename: string of (.csv) file containing nuclear positional information
-    *** :nodeweight: list of column name(s) strings in nuclear position dataframe (in 'filename'.csv) that contain node
+    *** :node_weight: list of column name(s) strings in nuclear position dataframe (in 'filename'.csv) that contain node
                      information (i.e. nuclear staining intensity, nuclear optimal density, cellular h-score value)
                      - the column name(s) list must contain less than 2 column names
-        :maxlinkingdistance(int): values of distance between nuclear centroids (in pixels) to form a link (edge) between
-        nuclei
+        :max_linking_distance: (int) value of distance between nuclear centroids (in pixels) to form a link (edge)
+                              between nuclei
+    """
 
-        """
-
-    def __init__(self, datadir, filename, nodeweight, maxlinkingdistance):
+    def __init__(self, data_dir, file_name, node_weight, max_link_distance):
         # Initializing preprocessing class object
         # automatically generates a pandas dataframe
-        self.datadir = datadir
-        self.filename = filename
-        self.df = pd.read_csv(f'{datadir}/{filename}.csv', index_col=0)
-        self.ld = maxlinkingdistance
-        self.nodeweight = nodeweight
+        self.data_dir = data_dir
+        self.file_name = file_name
+        self.df = pd.read_csv(f'{data_dir}/{file_name}.csv', index_col=0)
+        self.node_weight = node_weight
+        self.node_feature_count = len(self.node_weight)
+        self.ld = max_link_distance
 
-    def addedges(self):
+    def add_edges(self):
 
         """
-            addedges() generates networkx edge data linking nuclei that are 'linkingdistance' apart
+            add_edges() generates networkx edge data linking nuclei that are < 'max_linking_distance' apart
 
             :self: Preprocessing Object described above
-            :return: list containing networkx edge data [(node id1, node id2, lineardistance), ...]
-                            """
+            :return: list containing networkx edge data [(node id1, node id2, linear_distance), ...]
+        """
+
         # create a list of all possible x and y pairs
-        xlist = zip(self.df.index.values.tolist(), self.df['XCentroid'])
-        ylist = zip(self.df.index.values.tolist(), self.df['YCentroid'])
-        xpairs = [x for x in itertools.product(xlist, repeat=2)]
-        ypairs = [y for y in itertools.product(ylist, repeat=2)]
-        centroidpairs = zip(xpairs, ypairs)
+        x_list = zip(self.df.index.values.tolist(), self.df['XCentroid'])
+        y_list = zip(self.df.index.values.tolist(), self.df['YCentroid'])
+        x_pairs = [x for x in itertools.product(x_list, repeat=2)]
+        y_pairs = [y for y in itertools.product(y_list, repeat=2)]
+        centroid_pairs = zip(x_pairs, y_pairs)
 
         edges = []
 
-        for pair in centroidpairs:
+        for pair in centroid_pairs:
             # retrieve id data from each x, y pair
-            ids = pair[0][0][0], pair[0][1][0]
-            # calculate the linear distance between the centroid of two nodes in a 2D plane
-            dist = sqrt(((pair[0][1][1] - pair[0][0][1])**2) + ((pair[1][1][1] - pair[1][0][1])**2))
-            if ids[0] == ids[1]:
+            id1, id2 = pair[0][0][0], pair[0][1][0]
+
+            if id1 == id2:
                 continue
-            # generate 'edges' for each pair that are closer than the set 'maxlinkingdistance'
-            elif 0 < dist < self.ld:
-                edge = (str(ids[0]), str(ids[1]), {'length': dist})
-                edges.append(edge)
             else:
-                continue
+                # calculate the linear distance between the centroid of two nodes in a 2D plane
+                dist = sqrt(((pair[0][1][1] - pair[0][0][1]) ** 2) + ((pair[1][1][1] - pair[1][0][1]) ** 2))
+                # generate 'edges' for each pair that are closer than the set 'max_linking_distance'
+                if dist < self.ld:
+                    edge = (str(id1), str(id2), {'length': dist})
+                    edges.append(edge)
 
         return edges
 
-    def addnodes(self):
+    def add_nodes(self):
 
         """
-            addnodes() generates networkx node data from nuclear position and staining intensity values.
+            add_nodes() generates networkx node data from nuclear position and staining intensity values.
 
             :self: Preprocessing Object described above
             :return: list containing networkx node data [(node id, {weight: [], size: [], pos: ()}), ...]
-                    """
+        """
 
         # calculate centroid coordinates from [[Xmin, Xmax], [Ymin, Ymax]] values and store in self.df & in 'positions'
         self.df['XCentroid'] = (self.df.XMax + self.df.XMin) / 2
@@ -84,84 +87,83 @@ class MakeGraph:
 
         # store nodeweight(s) ('weight' and 'size') and position data for each node in a
         # list of nodes of form: [((ID),{'weight':[], ***'size':[], 'pos':[]}), ...]
+
+        if self.node_feature_count == 2:
+            stains = self.df[[self.node_weight[0], self.node_weight[1]]].values.tolist()
+        else:
+            stains = self.df[self.node_weight[0]].values.tolist()
+
+        nodeids = self.df.index.values.tolist()
+        nodedata = zip(nodeids, stains, positions)
+
         nodes = []
-        if len(self.nodeweight) == 2:
-            stains = self.df[[self.nodeweight[0], self.nodeweight[1]]].values.tolist()
-            nodeids = self.df.index.values.tolist()
-            nodedata = zip(nodeids, stains, positions)
-            for nodeid, stain, position in nodedata:
-                weight1 = stain[0]
+
+        for nodeid, stain, position in nodedata:
+            weight1 = stain[0]
+            if self.node_feature_count == 2:
                 weight2 = stain[1]
                 node = (str(nodeid), {'weight': weight1, 'size': weight2, 'pos': (int(position[0]), int(position[1]))})
-                nodes.append(node)
-        elif len(self.nodeweight) == 1:
-            stains = self.df[[self.nodeweight[0]]].values.tolist()
-            nodeids = self.df.index.values.tolist()
-            nodedata = zip(nodeids, stains, positions)
-            for nodeid, stain, position in nodedata:
-                weight1 = stain[0]
+            else:
                 node = (str(nodeid), {'weight': weight1, 'pos': (int(position[0]), int(position[1]))})
-                nodes.append(node)
-        else:
-            pass
+            nodes.append(node)
 
         return nodes
 
-    def graph(self, image='no'):
+    def graph(self, image=False, image_path=None, torch_graph=False):
 
         """
-            graph generates a networkx.Graph() from nuclear position (with staining intensity) 'Preprocessing' object.
+            graph generates a 'torch_geometric.data.data.Data' object from nuclear position (with staining intensity)
+            if torch parameter is set to True, otherwise, graph generates a networkx.Graph() object.
 
             :self: Preprocessing Object described above
-            :image: string of 'yes' or 'no' based on if you want an image of your graph returned
+            :image: set True if you want an image of networkx graph
+            :image_path: string with path to directory to save graph image
+            :torch_graph: set True if you want a torch_geometric.data.data.Data() object instead of networkx.Graph()
 
-            :return: 1. networkx.Graph() representing nuclear position (with staining intensity)
-                     2. PNG image file of 2D graph network with node weights represented as color and/or size of node
-                        image saved to 'datadir'/'filename'.png
+            :return: 1. (default): networkx.Graph()
+                        (if torch_graph = True): torch_geometric.data.data.Data()
+
+                     2. (if image = True): PNG image file of 2D graph network with node weights represented as color
+                        and/or size of node image saved to (default): 'datadir'/'filename'.png
+                                                           (if image_path specified): :image_path:/'filename'.png
+
             """
+
         # generate networkx.Graph() object and load in our generated nodes and edges data
         graph = nx.Graph()
-        graph.add_nodes_from(self.addnodes())
-        graph.add_edges_from(self.addedges())
-        #geograph = from_networkx(graph)
+        graph.add_nodes_from(self.add_nodes())
+        graph.add_edges_from(self.add_edges())
 
-        if image == 'yes' or image == 'Yes':
+        if image:
 
+            # images require both node position and 'weight' data ('size' captured below if len(:node_weights:) > 1
             pos = nx.get_node_attributes(graph, 'pos')
-            outpath = f'{self.datadir}/GraphImages/{self.filename}.png'
+            weight = nx.get_node_attributes(graph, 'weight')
 
-            if len(self.nodeweight) == 2:
-                # if there are two variables associated with a single node (i.e. GATA3 & CK5 expression),
-                # their data can be stored in two variable types 'size' and 'color' of the node
+            if image_path:
+                out_path = image_path + f'/{self.file_name}.png'
+            else:
+                out_path = f'{self.data_dir}/{self.file_name}.png'
+
+            # we will capture node feature information as colour (node weight) [and size (node size)]
+            if self.node_feature_count == 2:
                 size = nx.get_node_attributes(graph, 'size')
-                weight = nx.get_node_attributes(graph, 'weight')
-                # graph is displayed, saved, and returned as networkx.Graph object
                 nx.draw(graph, pos, node_color=list(weight.values()), node_size=list(size.values()))
-                if path.exists(f'{self.datadir}/GraphImages'):
-                    plt.savefig(outpath, format="PNG")
-                    return graph
-                else:
-                    mkdir(f'{self.datadir}/GraphImages')
-                    plt.savefig(outpath, format="PNG")
-                    return graph
-
-            elif len(self.nodeweight) == 1:
-                # if there is only one variable associated with each node (i.e. p16 expression)
-                # the data will be expressed as a variance in 'color' between each node in the graph
-                weight = nx.get_node_attributes(graph, 'weight')
-                # graph is displayed, saved, and returned as networkx.Graph object
+            elif self.node_feature_count == 1:
                 nx.draw(graph, pos, node_color=list(weight.values()))
 
-                if path.exists(f'{self.datadir}/GraphImages'):
-                    plt.savefig(outpath, format="PNG")
-                    return graph
-                else:
-                    mkdir(f'{self.datadir}/GraphImages')
-                    plt.savefig(outpath, format="PNG")
-                    return graph
+            plt.savefig(out_path)
 
-            else:
-                pass
+        if torch_graph:
+            # generate a torch_geometric.data.data.Data object if :torch_graph: = True
+            graph = from_networkx(graph)
 
-        else:
-            return graph
+        return graph
+
+
+matrix = MakeGraph('/Users/andrewgarven/PycharmProjects/SubtypePlasticity/Data',
+                   'TESTER-tma',
+                   ['Stain 1 Nucleus OD', 'Stain 2 Cytoplasm OD'],
+                   100).graph(image=True,
+                              image_path='/Users/andrewgarven/PycharmProjects/SubtypePlasticity/Data/GraphImages',
+                              torch_graph=True)
--- Funcs/SummaryStatistics.py
+++ Funcs/SummaryStatistics.py
@@ -1,54 +1,148 @@
 import pandas as pd
 from collections import defaultdict
+import matplotlib.pyplot as plt
 
 
-class Summary:
+class HALOSummary:
 
-    """MakeGraph is used to generate a networkx object from list of 2D coordinates with nodal information
+    """HALOSummary is used to generate a summary data from a HALO image analysis software output .csv file
 
         Typical use:
 
-            graph : MakeGraph(datadir, filename, nodeweight, maxlinkingdistance).graph()
+            report : HALOSummary(datadir, filename, stain1, stain2, loc1, loc2).report()
 
-        :datadir: string of directory containing nuclear position data
-        :filename: string of (.csv) file containing nuclear positional information
-    *** :nodeweight: list of column name(s) strings in nuclear position dataframe (in 'filename'.csv) that contain node
-                     information (i.e. nuclear staining intensity, nuclear optimal density, cellular h-score value)
-                     - the column name(s) list must contain less than 2 column names
-        :maxlinkingdistance(int): values of distance between nuclear centroids (in pixels) to form a link (edge) between
-        nuclei
+        returns a summarized report including the following:
+            1. histogram of the relative average optical density values on the Tissue Micro-Array (TMA)
+
+        Alt use:
+             plot_hist : HALOSummary(datadir, filename, stain1, stain2, loc1, loc2).plot_hist()
+
+        :datadir: [str] of directory containing nuclear position data
+        :filename: [str] of (.csv) file containing HALO image analysis output
+        :stain1: [str] name of first stain (ex. 'Gata3')
+        :stain2: [str] name of second stain (ex. 'Ck5')
+        :loc1: [str] cellular staining location nuclear: 'nuclear' or 'n',
+                                                cytoplasmic: 'cytoplasmic' or 'c'
+        :loc2: [str] cellular staining location nuclear: 'nuclear' or 'n',
+                                                cytoplasmic: 'cytoplasmic' or 'c'
 
         """
 
-    def __init__(self, datadir, filename):
+    def __init__(self, datadir, file_name, stain1='stain1', stain2='stain2', loc1='n', loc2='n', anno=True):
+
         # Initializing preprocessing class object
         # automatically generates a pandas dataframe
         self.datadir = datadir
-        self.filename = filename
-        self.df = pd.read_csv(f'{datadir}/{filename}.csv', index_col=0)
+        self.file_name = file_name
+        self.df = pd.read_csv(f'{datadir}/{file_name}.csv', index_col=0)
+        self.stain1 = stain1
+        self.stain2 = stain2
+        self.loc1 = loc1
+        self.loc2 = loc2
+        self.stain_data = defaultdict(dict)
 
-        staindata = defaultdict(dict)
-        annotatedCellData = self.df[self.df['Analysis Region'] == 'Layer 1'].values.tolist()
-        for celldata in annotatedCellData:
-            spotid = celldata[10]
-            coreid = celldata[6]
-            totalcells = celldata[16]
-            Gata0 = celldata[18]
-            Gata1 = celldata[19]
-            Gata2 = celldata[20]
-            KRT0 = celldata[22]
-            KRT1 = celldata[23]
-            KRT2 = celldata[24]
-            Both = celldata[25]
-            Neither = celldata[26]
+        if anno:
+            cell_data = self.df[self.df['Analysis Region'] == 'Layer 1'].values.tolist()
+        else:
+            cell_data = self.df[self.df['Analysis Region'] == 'entire image'].values.tolist()
 
-            staindata[f'{spotid}'][f'{coreid}'] = {'totalcells': totalcells,
-                                                   'gata3': [Gata0, Gata1, Gata2],
-                                                   'ck5': [KRT0, KRT1, KRT2],
-                                                   'Dual Positive': Both,
-                                                   'Dual Negative': Neither}
-        self.celldata = dict(staindata)
+        for tma in cell_data:
+            spot_id = tma[10]
+            core_id = tma[6]
+            total_cells = tma[16]
+
+            stain1_0 = tma[18]
+            stain1_1 = tma[19]
+            stain1_2 = tma[20]
+            stain1_3 = tma[21]
+            stain1_h_score = tma[34]
+
+            stain2_0 = tma[23]
+            stain2_1 = tma[24]
+            stain2_2 = tma[25]
+            stain2_3 = tma[26]
+            stain2_h_score = tma[40]
+
+            dual_positive = tma[27]
+            dual_negative = tma[28]
+
+            if loc1.lower() in ['cytoplasmic', 'c']:
+                stain1_optical_density = tma[45]
+            else:
+                stain1_optical_density = tma[43]
+
+            if loc2.lower() in ['cytoplasmic', 'c']:
+                stain2_optical_density = tma[46]
+            else:
+                stain2_optical_density = tma[44]
+
+            self.stain_data[f'{spot_id}'][f'{core_id}'] = {'total_cells': total_cells,
+                                                           self.stain1: {'score_count': [stain1_0,
+                                                                                         stain1_1,
+                                                                                         stain1_2,
+                                                                                         stain1_3],
+                                                                         'h_score': stain1_h_score,
+                                                                         'optical_density': stain1_optical_density},
+                                                           self.stain2: {'score_count': [stain2_0,
+                                                                                         stain2_1,
+                                                                                         stain2_2,
+                                                                                         stain2_3],
+                                                                         'h_score': stain2_h_score,
+                                                                         'optical_density': stain2_optical_density},
+                                                           'dual_positive': dual_positive,
+                                                           'dual_negative': dual_negative}
+
+        self.stain_data = dict(self.stain_data)
+        print(self.stain_data['211'])
+
+    def plot_hist(self, save_image=False, image_dir=None):
+
+        """
+            plot_hist() generates a matplotlib.pyplot.hist() plot from HALO optical density values
+
+            :self: HALOSummary Object described above
+            :save_image: set True if you would like a png image of histogram
+            :image_path: [str] contain path to directory to save png image file
+
+            :return: 1. (default) matplotlib.pyplot.hist() plot
+                     2. (if image = True): PNG image file of histogram
+                        and/or size of node image saved to (default): 'datadir'/'filename'.png
+                                                           (if image_path specified): :image_path:/'filename'.png
+        """
+
+        stain_1 = []
+        stain_2 = []
+        plot_title = f'Optical Density Histogram [{self.stain1} and {self.stain2}]'
+        for patient in list(self.stain_data.keys()):
+            patient_stain1 = []
+            patient_stain2 = []
+            for tma in list(self.stain_data[patient].keys()):
+                od1 = self.stain_data[patient][tma][self.stain1]['optical_density']
+                patient_stain1.append(od1)
+                od2 = self.stain_data[patient][tma][self.stain2]['optical_density']
+                patient_stain2.append(od2)
+            stain_1.extend(patient_stain1)
+            stain_2.extend(patient_stain2)
+
+        plt.hist(stain_1, alpha=0.5, label=self.stain1)
+        plt.hist(stain_2, alpha=0.5, label=self.stain2)
+        plt.title(plot_title)
+        plt.xlabel('Optical Density Value')
+        plt.ylabel('Number of Patients')
+        plt.legend(loc='upper right')
+
+        if save_image:
+            if image_dir:
+                plt.savefig(f'{image_dir}/{self.file_name}.png')
+            else:
+                plt.savefig(f'{self.datadir}/{self.file_name}.png')
+        else:
+            plt.show()
 
 
-Summary('/Users/andrewgarven/PycharmProjects/SubtypePlasticity/Data', 'TMA1_GATA3CK5_summary')
-
+HALOSummary('/Users/andrewgarven/PycharmProjects/SubtypePlasticity/Data',
+            'TMA1_GATA3CK5_summary',
+            stain1='Gata3',
+            stain2='Ck5',
+            loc1='n',
+            loc2='c')
